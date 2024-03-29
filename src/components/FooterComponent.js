2c52b3fa5e162248e835bd19c7379d329373a162
SHRUTHI DUDDEDA <shruthiduddeda88@gmail.com>
2024-03-29 12:04:35 +0530

    myProject

---
+++ src/components/FooterComponent.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+	export default class FooterComponent extends Component {
+ 	 render() {
+    	return (
+      	<div>
+        	<footer className='footer'>
+       <span>All Right Reserved 2023 &copy;shruthi </span>
+        </footer>
+     	 </div>
+    	)
+  	}
+	}
